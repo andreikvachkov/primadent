52feccec00693f35e01888054102df9f013967be
pixelstudioweb <154566274+pixelstudioweb@users.noreply.github.com>
2024-08-08 21:45:28 +0700

    6
    33

---
+++ assets/js/gsap-about.js
@@ -0,0 +1,46 @@
+window.addEventListener('load', function () {
+
+    console.log('GSAP loaded');
+
+    gsap.to(".about-info__bg", {
+        yPercent: 200,
+        scale: 3,
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".about-info",
+            start: "top bottom",
+            end: "bottom top",
+            scrub: true,
+        },
+    });
+
+
+    const items2 = gsap.utils.toArray(".about_about__item");
+    const itemHeight2 = items2[0].offsetHeight;
+    const containerHeight = document.querySelector(".about_about__left").offsetHeight;
+    const totalHeight2 = itemHeight2 * (items2.length - 1);
+    const isMobile = window.innerWidth <= 768;
+    const tl2 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".about_about",
+            start: isMobile ? `+=${containerHeight}px` : `-=${itemHeight2 / 3}px`,
+            end: `+=${totalHeight2}px`,
+            scrub: true,
+            pin: true,
+        }
+    });
+    
+    gsap.set(items2[0], { y: 0 });
+    
+    items2.slice(1).forEach((item, index) => {
+        tl2.fromTo(item,
+            { y: '200%' }, 
+            { y: 0, ease: "none" }, 
+            index * 0.5
+        );
+    });
+
+
+
+
+});
--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -1,4 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('load', function () {
+
     console.log('GSAP loaded');
 
     gsap.to(".services__bg1", {
@@ -10,12 +11,11 @@ document.addEventListener('DOMContentLoaded', () => {
             start: "top bottom",
             end: "bottom top",
             scrub: true,
-            markers: true
         },
     });
 
     gsap.to(".services__bg2", {
-        yPercent: 1500,
+        yPercent: 1000,
         scale: 5.5,
         ease: "none",
         scrollTrigger: {
@@ -23,7 +23,6 @@ document.addEventListener('DOMContentLoaded', () => {
             start: "top bottom",
             end: "bottom top",
             scrub: true,
-            markers: true
         },
     });
 
@@ -36,11 +35,43 @@ document.addEventListener('DOMContentLoaded', () => {
             start: "top bottom",
             end: "bottom top",
             scrub: true,
-            markers: true
         },
     });
     gsap.to(".services__bg4", {
-        yPercent: 200,
+        yPercent: 1000,
+        scale: 2.3,
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".services",
+            start: "top bottom",
+            end: "bottom top",
+            scrub: true,
+        },
+    });
+    gsap.to(".services__bg5", {
+        yPercent: 2000,
+        scale: 6,
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".services",
+            start: "top bottom",
+            end: "bottom top",
+            scrub: true,
+        },
+    });
+    gsap.to(".services__bg6", {
+        yPercent: 700,
+        scale: 10.3,
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".services",
+            start: "top bottom",
+            end: "bottom top",
+            scrub: true,
+        },
+    });
+    gsap.to(".services__bg7", {
+        yPercent: 500,
         scale: 0.3,
         ease: "none",
         scrollTrigger: {
@@ -48,26 +79,98 @@ document.addEventListener('DOMContentLoaded', () => {
             start: "top bottom",
             end: "bottom top",
             scrub: true,
-            markers: true
         },
     });
+    gsap.to(".services__bg8", {
+        yPercent: 300,
+        scale: 0.4,
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".services",
+            start: "top bottom",
+            end: "bottom top",
+            scrub: true,
+        },
+    });
+    gsap.to(".services__bg9", {
+        yPercent: 300,
+        scale: 2.7,
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".services",
+            start: "top bottom",
+            end: "bottom top",
+            scrub: true,
+        },
+    });
+    gsap.to(".about-info__bg", {
+        yPercent: 200,
+        scale: 3,
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".about-info",
+            start: "top bottom",
+            end: "bottom top",
+            scrub: true,
+        },
+    });
+    // gsap.registerPlugin(ScrollTrigger);
+
+    // const items = gsap.utils.toArray(".about__item");
+    // const itemHeight = items[0].offsetHeight;
+    // const totalHeight = (itemHeight + 20) * items.length; 
+
+    // function logPositions() {
+    //     items.forEach((item, index) => {
+    //         const computedTransform = window.getComputedStyle(item).transform;
+    //         const matrix = new WebKitCSSMatrix(computedTransform);
+    //         console.log(`Блок ${index + 1}: смещение transform = ${matrix.m42}px`);
+    //     });
+    // }
+
+    // gsap.fromTo(items,
+    //     { y: (i) => i * itemHeight },
+    //     {
+    //         y: 0,
+    //         ease: "none",
+    //         scrollTrigger: {
+    //             trigger: ".about",
+    //             start: "top top",
+    //             end: `+=${totalHeight}px`,
+    //             scrub: true,
+    //             pin: true,
+    //             markers: true,
+    //             onUpdate: logPositions
+    //         }
+    //     }
+    // );
+
+
     gsap.registerPlugin(ScrollTrigger);
 
     const items = gsap.utils.toArray(".about__item");
-
-    gsap.fromTo(items,
-        { top: (i) => `${i * 110}%` },
-        {
-            top: "0%",
-            ease: "none",
-            scrollTrigger: {
-                trigger: ".about",
-                start: "top top",
-                end: "+=" + (items.length * 110) + "%",
-                scrub: true,
-                pin: true,
-                markers: true // Включите для отладки
-            }
+    const itemHeight = items[0].offsetHeight;
+    const totalHeight = itemHeight * (items.length - 1);
+    const isMobile = window.innerWidth <= 768;
+    const tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".about",
+            start: isMobile ? `+=${itemHeight}px` : "top top" ,
+            end: `+=${totalHeight}px`,
+            scrub: true,
+            pin: true,
         }
-    );
+    });
+    
+    gsap.set(items[0], { y: 0 });
+    
+    items.slice(1).forEach((item, index) => {
+        tl.fromTo(item,
+            { y: '200%' }, 
+            { y: 0, ease: "none" }, 
+            index * 0.5
+        );
+    });
+
+
 });
